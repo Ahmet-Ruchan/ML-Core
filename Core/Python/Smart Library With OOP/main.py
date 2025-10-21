650c9ea4d5dd5ff5d92b33974dbae73110a431e1
Ahmet Ruçhan Avcı <aruchanavci01@gmail.com>
2025-10-22 00:37:33 +0300

    push 22.10.2025-Ver3.9

--- Core/Python/Smart Library With OOP/main.py
+++ Core/Python/Smart Library With OOP/main.py
@@ -1,20 +1,26 @@
-from Book import Book
-
+from SaveBook import SaveBook
 
 if __name__ == "__main__":
+
     print("This is Smart Library.")
 
-Book_Crime_and_Punishment = Book("Crime and Punishment", "Fyodor Dostoevsky", "978-0140449136", "Fiction", 10)
+    # Book_Crime_and_Punishment = SaveBook("Crime and Punishment", "Fyodor Dostoevsky", "978-0140449136", "Fiction", 10)
+    #
+    # ISBN = Book_Crime_and_Punishment.get_ISBN()
+    # print("ISBN:", ISBN)
+    #
+    # current_stock = Book_Crime_and_Punishment.get_stock()
+    # print("Current Stock:", current_stock)
+    #
+    # print("Changing Stock...")
+    # Book_Crime_and_Punishment.set_stock(15)
+    # print("New Stock Is:", Book_Crime_and_Punishment.get_stock())
+    #
+    # print("General Information of the Book:")
+    # print(Book_Crime_and_Punishment)
 
-ISBN = Book_Crime_and_Punishment.get_ISBN()
-print("ISBN:", ISBN)
+    print("Welcome to Smart Library System.")
 
-current_stock = Book_Crime_and_Punishment.get_stock()
-print("Current Stock:", current_stock)
-
-print("Changing Stock...")
-Book_Crime_and_Punishment.set_stock(15)
-print("New Stock Is:", Book_Crime_and_Punishment.get_stock())
-
-print("General Information of the Book:")
-print(Book_Crime_and_Punishment)
+    save = SaveBook()
+    save.save_book_info()
+    save.display_saved_books()
